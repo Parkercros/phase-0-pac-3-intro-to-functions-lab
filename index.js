f85c6f10a0d78fc46a9cde79f4d247469e064235
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-22 18:08:59 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function shout(string) {
-    // todo
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string == string.toLowerCase()){
+      return "I can't hear you!";
   }
-function shout(string) {
-    return String();
+  else if (string == string.toUpperCase()){
+      return "YES INDEED!";
   }
-  "Hello!".toUpperCase(); // 'HELLO!'
-  function shout(string) {
-    return string.toUpperCase();
+  else if (string == "Let's have dinner together!"){
+      return "I would love to!";
   }
-  function whisper(string) {
-    return string.toLowerCase()
-  }
+}
 
